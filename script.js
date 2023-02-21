c0a344886ae11cbd4b2c372db1e9eda83cb65f5c
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-02-21 22:02:10 +0400

    javascript variables, primitive data types

---
+++ script.js
@@ -0,0 +1,96 @@
+// console.log("hello from js file");
+
+// var userName = "alina";
+let userSurname = "bregvadze";
+// let USERSURNAME = "bregvadze";
+// const 2userName = 'alina';
+// const user-Name = 'alina';
+// const const  = 'alina';
+const age = 26;
+// userEmail = "example@gmail.com";
+const productPrice = 280;
+let totalPrice = 0;
+// console.log(userSurname, age);
+totalPrice = 180;
+// console.log("after price change", totalPrice);
+
+//string
+let myText = "example text";
+// const myText2 = 'example text';
+// const myText3 = `example
+
+// text`;
+// console.log("typeof myText", typeof myText);
+
+// myText = 24;
+// console.log("type of example text", typeof "example text");
+// console.log("typeof myText", typeof myText);
+
+//number
+const myNumber = 10;
+const myNumber2 = 20.4;
+
+// console.log("typeof myNumber", typeof myNumber);
+
+//boolean
+
+const myBool1 = true;
+const myBool2 = false;
+
+// console.log("typeof myBool1", typeof myBool1);
+
+//undefined
+let myUndefined;
+let myUndefined2 = undefined;
+// const deliveryPrice;
+
+/*
+console.log("typeof myUndefined", typeof myUndefined);
+deliveryPrice = 10;
+deliveryPrice = 0;
+*/
+
+//null
+let myNull = null;
+// console.log("typeof myNull", typeof myNull);
+
+const myNum1 = 10;
+const myNum2 = 25;
+const myNum3 = 3;
+
+const sum = myNum1 + myNum2;
+console.log(sum, "sum");
+
+const substract = myNum2 - myNum1;
+console.log(substract, "substract");
+
+const division = myNum2 / myNum1;
+console.log(division, "division");
+
+const multiply = myNum1 * myNum2;
+console.log(multiply, "multiply");
+
+const modulo = myNum2 % myNum1;
+console.log(modulo, "modulo");
+
+const myText1 = "example text";
+const myText2 = "new text";
+
+//NaN not a number
+console.log(myNum1 - myText1);
+console.log(typeof (myNum1 - myText1));
+
+const result1 = myNum1 + myNum2 + myText;
+console.log(result1, " myNum1 + myNum2 + myText");
+
+const result2 = myNum1 + myText + myNum2;
+console.log(result2, " myNum1 + myText + myNum2");
+
+const result3 = myText + myNum1 + myNum2;
+console.log(result3, "myText + myNum1 + myNum2");
+
+const myText4 = "my last name is " + userSurname + " and my name is ...";
+console.log(myText4);
+
+const myText5 = `my last name is ${userSurname} and my age is ${2023 - 1996}`;
+console.log(myText5);
